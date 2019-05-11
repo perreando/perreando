f055610070d2a00d40a46cb860f97b67897f6a8c
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-05-11 12:30:21 +0200

    comments fixed

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const secure = require('../middlewares/secure.mid');
 const users = require('../controllers/users.controller');
 const comments = require('../controllers/comments.controller');
+const walks = require('../controllers/walks.controller');
+
 
 router.get('/', secure.isAuthenticated, users.list);
 router.get('/:id', users.details);
 router.post('/comments', secure.isAuthenticated, comments.doCreate)
+
+
 //router.post('/:id/delete', secure.isAuthenticated, users.delete);
 
 module.exports = router;
