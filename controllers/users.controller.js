db6ff6a5f027509401765998a911faae942fed84
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-05-12 19:25:34 +0200

    add search form to list

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -8,6 +8,27 @@ const mongoose = require('mongoose');
 //   .then((users) => res.json(users.map(u => u.location)))
 //   .catch(next)
 // }
+
+module.exports.list = (req, res, next) => {
+  const criteria = {};
+
+  if (req.query.name) {
+    criteria.name = new RegExp(req.query.name, 'i');
+  }
+
+  User.find(criteria)
+    .sort({ _id: 1 })
+    .then(users => res.render('users/list', { 
+      users,
+      name: req.query.name 
+    }))
+    .catch(error => next(error));
+}
+
+
+
+
+
 module.exports.details = (req, res, next) => {
   const id = req.params.id;
 
@@ -26,10 +47,10 @@ module.exports.details = (req, res, next) => {
   }
 }
 
-module.exports.list = (req, res, next) => {
-  User.find()
-    .then(users => {
-      res.render('users/list', { users });
-    })
-    .catch(error => next(error))
-}   
+// module.exports.list = (req, res, next) => {
+//   User.find()
+//     .then(users => {
+//       res.render('users/list', { users });
+//     })
+//     .catch(error => next(error))
+// }   
